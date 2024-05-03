c7bd2217a8e26f1092cf0df59c8d8b7e2c062e1b
Wardrek <wardrek.hugo@gmail.com>
2024-05-03 14:04:59 +0200

    Textobject + telescope help tags

--- .config/nvim/lua/wardrek/keymaps.lua
+++ .config/nvim/lua/wardrek/keymaps.lua
@@ -146,6 +146,18 @@ nnoremap('<leader>fg', telescope.git_files, {})
 nnoremap('<leader>fb', telescope.buffers, {})
 nnoremap('<leader>fr', telescope.oldfiles, {})
 nnoremap('<leader>fl', telescope.current_buffer_fuzzy_find, {})
+nnoremap('<leader>fh', telescope.help_tags, {})
+
+-- Treesitter
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+-- vim way: ; goes the direcation you were moving
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+-- Make builtin f, F, t, T also repeatable with ; and ,
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
 
 -- Trouble
 nnoremap('<leader>tt', function()
